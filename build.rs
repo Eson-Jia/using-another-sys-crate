9d96a75c08598d2063bc21c056bf8ef3b7921458
jiayuxin <13520723393@163.com>
2021-11-18 17:47:13 +0800

    对 build.rs 进行修改后可以编译通过并成功运行

--- build.rs
+++ build.rs
@@ -4,6 +4,9 @@ fn main() {
     if let Some(include) = std::env::var_os("DEP_Z_INCLUDE") {
         cfg.include(include);
     }
+    cfg.shared_flag(true);
+    cfg.static_flag(false);
     cfg.compile("zuser");
     println!("cargo:rerun-if-changed=src/zuser.c");
+    pkg_config::Config::new().probe("zlib").unwrap();
 }
